0d47e2001c2b4a1babd5c5dce5f31dd590df727d
smirnovs <maneerlik@ya.ru>
2025-09-16 23:29:41 +0300

    1-collector-json: заменил аннотацию @FieldDefaults явными модификаторами доступа

--- telemetry/collector/src/main/java/ru/practicum/collector/model/sensor/ClimateSensorEvent.java
+++ telemetry/collector/src/main/java/ru/practicum/collector/model/sensor/ClimateSensorEvent.java
@@ -1,17 +1,14 @@
 package ru.practicum.collector.model.sensor;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
 @ToString(callSuper = true)
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ClimateSensorEvent extends SensorEvent {
-    int temperatureC;
-    int humidity;
-    int co2Level;
+    private int temperatureC;
+    private int humidity;
+    private int co2Level;
 }
--- telemetry/collector/src/main/java/ru/practicum/collector/model/sensor/MotionSensorEvent.java
+++ telemetry/collector/src/main/java/ru/practicum/collector/model/sensor/MotionSensorEvent.java
@@ -1,17 +1,14 @@
 package ru.practicum.collector.model.sensor;
 
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
 @ToString(callSuper = true)
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class MotionSensorEvent extends SensorEvent {
-    int linkQuality;
-    boolean motion;
-    int voltage;
+    private int linkQuality;
+    private boolean motion;
+    private int voltage;
 }
