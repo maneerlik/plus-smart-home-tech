931a53d4c2a0733f0b076d320d5945e61f543796
smirnovs <maneerlik@ya.ru>
2025-09-28 19:00:18 +0300

    3-aggregator: Реализовал логику аггрегатора

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/service/AggregatorRunner.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AggregatorRunner implements CommandLineRunner {
+    private final SnapshotProcessor snapshotProcessor;
+
+
+    @Override
+    public void run(String... args) {
+        snapshotProcessor.start();
+    }
+}
