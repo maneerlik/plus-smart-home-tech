4531e22d9376e9e68e77e0d36e94233140236fdf
smirnovs <maneerlik@ya.ru>
2025-10-18 16:00:25 +0300

    7-spring-cloud-microservices: исправил конфиг analizer, исправил инициализацию контейнера db - теперь контейнер крутит сразу все БД, добавил initdb.sql - создающий все БД

---
+++ initdb/01-create-dbs.sql
@@ -0,0 +1,5 @@
+-- Создает БД при первом запуске контейнера
+CREATE DATABASE "analyzer-db";
+CREATE DATABASE "shopping-store-db";
+CREATE DATABASE "shopping-cart-db";
+CREATE DATABASE "warehouse-db";
